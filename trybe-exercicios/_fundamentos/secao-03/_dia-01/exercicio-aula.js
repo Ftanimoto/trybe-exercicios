ca64aa5631b67ce84db0b7cfe31b97620bc5ca89
Felipe Tanimoto de Albuquerque <felipetanimoto@gmail.com>
2023-05-02 16:51:34 -0300

    criando a branch da seção-3 e fazendo o primeiro commit da branch

---
+++ trybe-exercicios/_fundamentos/secao-03/_dia-01/exercicio-aula.js
@@ -0,0 +1,21 @@
+let num1 = 5;
+let num2 = 15;
+
+// Soma
+let soma = num1 + num2;
+console.log(soma);
+
+// ou 
+
+soma;
+
+
+// Multiplicação
+
+let mult = num1*num2;
+console.log(mult);
+
+//Divisão
+
+let div = num1 / num2;
+
